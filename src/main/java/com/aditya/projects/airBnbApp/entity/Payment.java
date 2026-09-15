480b91643ba3e4af360c17b63cea20aec68e584d
addyiez <88774507+addyiez@users.noreply.github.com>
2026-09-15 17:25:44 +0530

    adding timestamp in payment

--- src/main/java/com/aditya/projects/airBnbApp/entity/Payment.java
+++ src/main/java/com/aditya/projects/airBnbApp/entity/Payment.java
@@ -4,8 +4,11 @@ import com.aditya.projects.airBnbApp.entity.enums.PaymentStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -25,4 +28,10 @@ public class Payment {
 
     @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal amount;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
